b2071e3dfc97557c45cdd07c6951ba57094f3467
sjyb9394 <69370122+sjyb9394@users.noreply.github.com>
2021-03-16 17:11:48 -0400

    Create Trees: Is This a Binary Search Tree?.py

---
+++ Tree/Trees: Is This a Binary Search Tree?.py
@@ -0,0 +1,22 @@
+""" Node is defined as
+class node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+"""
+
+def checkBST(root):
+    min_left = -9999999
+    max_right = 9999999
+    def check(node, left, right):
+        if node is None:
+            return True
+        if node.data <= left:
+            return False
+        if node.data >= right:
+            return False
+        return check(node.left, left, node.data) and check(node.right, node.data, right)
+    
+    return check(root, min_left, max_right)
+        
